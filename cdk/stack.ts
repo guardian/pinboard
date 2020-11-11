40ae0a7354fbb08dd60b055d9f97353c84984ff2
Tom Richards <tom.richards@guardian.co.uk>
2020-11-11 18:51:19 +0000

    add apigateway to cdk file

--- cdk/stack.ts
+++ cdk/stack.ts
@@ -1,6 +1,8 @@
-import {CfnParameter, Construct, Duration, IConstruct, Stack, StackProps, Tag, Tags} from "@aws-cdk/core";
+import {CfnParameter, Construct, Duration, IConstruct, Stack, StackProps, Tags} from "@aws-cdk/core";
 import * as lambda from "@aws-cdk/aws-lambda";
 import * as S3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
+import * as apigateway from "@aws-cdk/aws-apigateway";
 
 export class PinBoardStack extends Stack {
 
@@ -35,7 +37,7 @@ export class PinBoardStack extends Stack {
     );
 
     const bootstrappingLambdaBasename = "pinboard-bootstrapping-lambda"
-    const bootstrappingLambdaAPIFunction = withStandardGuardianTags(
+    const bootstrappingLambdaFunction = withStandardGuardianTags(
       new lambda.Function(thisStack, bootstrappingLambdaBasename, {
         runtime: lambda.Runtime.NODEJS_12_X, // if changing should also change .nvmrc (at the root of repo)
         memorySize: 128,
@@ -53,6 +55,53 @@ export class PinBoardStack extends Stack {
         )
       })
     );
+
+    const bootstrappingLambdaPolicyStatement = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["execute-api:Invoke"],
+      resources: ["*"] //TODO change this to point to the lambda
+    });
+    bootstrappingLambdaPolicyStatement.addAnyPrincipal();
+
+    const bootstrappingApiGateway = new apigateway.LambdaRestApi(thisStack, `${bootstrappingLambdaBasename}-api`, {
+      handler: bootstrappingLambdaFunction,
+      endpointTypes: [apigateway.EndpointType.EDGE],
+      policy: new iam.PolicyDocument({
+        statements: [bootstrappingLambdaPolicyStatement]
+      }),
+      defaultMethodOptions: {
+        apiKeyRequired: false
+      }
+    });
+
+    // TODO add certificate etc. e.g...
+    // const telemetryCertificate = acm.Certificate.fromCertificateArn(
+    //   this,
+    //   "user-telemetry-certificate",
+    //   telemetryCertificateArn.valueAsString
+    // );
+    // const telemetryCertificate = acm.Certificate.fromCertificateArn(
+    //   this,
+    //   "user-telemetry-certificate",
+    //   telemetryCertificateArn.valueAsString
+    // );
+    //
+    // const telemetryDomainName = new apigateway.DomainName(
+    //   this,
+    //   "user-telemetry-domain-name",
+    //   {
+    //     domainName: telemetryHostName.valueAsString,
+    //     certificate: telemetryCertificate,
+    //     endpointType: apigateway.EndpointType.EDGE,
+    //   }
+    // );
+    //
+    // telemetryDomainName.addBasePathMapping(telemetryApi, { basePath: "" });
+    //
+    // new CfnOutput(this, "user-telemetry-api-target-hostname", {
+    //   description: "hostname",
+    //   value: `${telemetryDomainName.domainNameAliasDomainName}`,
+    // });
 
   }
 }

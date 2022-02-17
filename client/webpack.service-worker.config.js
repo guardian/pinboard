354d49a179e956677f5973a44aecf3c4b6d5ef29
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-02-17 12:35:06 +0000

    push-notifications: use webpack-html-plugin to generate html file

--- client/webpack.service-worker.config.js
+++ client/webpack.service-worker.config.js
@@ -1,20 +1,32 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry: "./src/push-notifications/index.html",
+  entry: "./src/push-notifications/registerServiceWorker.ts",
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
+        test: /\.ts$/,
         use: "ts-loader",
-        exclude: /node_modules/,
-      },
+        exclude: [
+          /node_modules/,
+          /dist/
+        ]
+      }
     ],
   },
   resolve: {
     extensions: [".ts", ".js"],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/push-notifications/index.html',
+      scriptLoading: 'blocking'
+    })
+  ],
   output: {
+    assetModuleFilename: "[name][ext]",
+    clean: true,
     publicPath: ".",
     path: path.resolve(__dirname, "dist/push-notifications"),
   },

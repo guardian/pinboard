6a144135dc9ee680a821df3fa0f4077dbb031725
Tom Richards <tom.richards@guardian.co.uk>
2022-03-02 12:19:02 +0000

    fix bundle-analyzer, now that we use function for webpack config
    Co-Authored-By: Andrew Nowak <andrew.nowak@guardian.co.uk>

--- client/webpack-bundle-analyzer.config.js
+++ client/webpack-bundle-analyzer.config.js
@@ -2,10 +2,11 @@ const { merge } = require("webpack-merge");
 const mainClient = require("./webpack.config.js");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
-module.exports = merge(mainClient, {
-  plugins: [
-    new BundleAnalyzerPlugin({
-      analyzerMode: "static",
-    }),
-  ],
-});
+module.exports = (env, argv) =>
+  merge(mainClient(env, argv), {
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: "static",
+      }),
+    ],
+  });

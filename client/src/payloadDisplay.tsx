0330d61f349cb0a2381f4beb0e7f75c1d3a26975
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-03-11 14:52:10 +0000

    allow images to resize to fill their container

--- client/src/payloadDisplay.tsx
+++ client/src/payloadDisplay.tsx
@@ -5,18 +5,14 @@ import { palette, space } from "@guardian/source-foundations";
 import { SvgCross } from "@guardian/source-react-components";
 import { buttonBackground } from "./styling";
 
-const defaultPayloadHeightPx = 150;
-
 interface PayloadDisplayProps extends PayloadAndType {
   clearPayloadToBeSent?: () => void;
-  heightPx?: number;
 }
 
 export const PayloadDisplay = ({
   // type, TODO consider tweaking display based on type
   payload: { thumbnail, embeddableUrl },
   clearPayloadToBeSent,
-  heightPx,
 }: PayloadDisplayProps) =>
   thumbnail && embeddableUrl ? (
     <div
@@ -25,13 +21,13 @@ export const PayloadDisplay = ({
         flex-direction: row;
         position: relative;
         padding: ${space[1]}px;
-        max-width: fit-content;
       `}
     >
       <img // TODO: hover for larger thumbnail
         src={thumbnail}
         css={css`
-          max-height: ${heightPx ?? defaultPayloadHeightPx}px;
+          max-width: 100%;
+          max-height: 350px;
         `}
         onDragStart={(event) =>
           event.dataTransfer.setData("URL", embeddableUrl)

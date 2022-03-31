5891a19399bbec68c801ddd9a4a9b39326b65ecb
Tom Richards <tom.richards@guardian.co.uk>
2022-03-31 10:12:11 +0100

    move display of `NotTrackedInWorkflow` into `SelectPinboard` (rather than it being its own page, that one couldn't navigate up from)

--- client/src/notTrackedInWorkflow.tsx
+++ client/src/notTrackedInWorkflow.tsx
@@ -3,7 +3,7 @@ import { standardPanelContainerCss } from "./styling";
 
 export const NotTrackedInWorkflow = () => (
   <div css={standardPanelContainerCss}>
-    In order to use Pinboard, please:
+    In order to use Pinboard with this piece, please:
     <ol>
       <li>Track this piece in Workflow using the left side panel</li>
       <li>Refresh the page</li>

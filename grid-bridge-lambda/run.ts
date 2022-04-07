77e1eb28b13cf1ec6cd6d4997e6f64c3f31beee6
Andrew Nowak <andrew.nowak@guardian.co.uk>
2022-04-07 10:37:27 +0100

    Create grid-bridge-lambda
    Co-authored-by: Tom Richards <tom.richards@guardian.co.uk>

---
+++ grid-bridge-lambda/run.ts
@@ -0,0 +1,10 @@
+import { handler } from "./src";
+
+handler({
+  arguments: {
+    apiUrl:
+      "https://api.media.test.dev-gutools.co.uk/images?q=%23Test1&length=1&orderBy=-uploadTime",
+  },
+})
+  .then(console.log)
+  .catch(console.error);

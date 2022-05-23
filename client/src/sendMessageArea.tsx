7551f59844348b79a5ef6d009a75466aa875958a
Ara_cho <47318984+Aracho1@users.noreply.github.com>
2022-05-23 17:35:23 +0100

    tracks messages sent and message types
    Message types: grid assets, message-only or has mentions

--- client/src/sendMessageArea.tsx
+++ client/src/sendMessageArea.tsx
@@ -1,7 +1,7 @@
 import { ApolloError, useMutation } from "@apollo/client";
 import { css } from "@emotion/react";
 import { palette, space } from "@guardian/source-foundations";
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Item, User } from "../../shared/graphql/graphql";
 import { gqlCreateItem } from "../gql";
 import { CreateItemInputBox } from "./createItemInputBox";
@@ -11,6 +11,7 @@ import { userToMentionHandle } from "./util";
 import { composer } from "../colours";
 import SendArrow from "../icons/send.svg";
 import { buttonBackground } from "./styling";
+import { TelemetryContext, PINBOARD_TELEMETRY_TYPE } from "./types/Telemetry";
 
 interface SendMessageAreaProps {
   payloadToBeSent: PayloadAndType | null;
@@ -40,11 +41,18 @@ export const SendMessageArea = ({
     .filter((user) => message.includes(userToMentionHandle(user)))
     .map((user) => user.email);
 
+  const sendTelemetryEvent = useContext(TelemetryContext);
+
   const [sendItem] = useMutation<{ createItem: Item }>(gqlCreateItem, {
     onCompleted: (sendMessageResult) => {
       onSuccessfulSend({
         ...sendMessageResult.createItem,
         pending: true,
+      });
+      sendTelemetryEvent?.(PINBOARD_TELEMETRY_TYPE.MESSAGE_SENT, {
+        pinboardId: sendMessageResult.createItem.pinboardId,
+        messageType: payloadToBeSent?.type || "message-only",
+        hasMentions: verifiedMentionEmails.length > 0,
       });
       setMessage("");
       clearPayloadToBeSent();

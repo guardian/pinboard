d506b6b8bb6571f1024e47311052e0c8bb67bbb2
Tom Richards <tom.richards@guardian.co.uk>
2022-09-07 17:34:56 +0100

    finish `runMigration.ts` to cover all tables (including GUID to sequential id mapping)

--- database-bridge-lambda/migration/runMigration.ts
+++ database-bridge-lambda/migration/runMigration.ts
@@ -1,20 +1,21 @@
 import AWS from "aws-sdk";
 import { DocumentClient } from "aws-sdk/clients/dynamodb";
-import { getDatabaseConnection } from "../../shared/database/databaseConnection";
 import { standardAwsConfig } from "../../shared/awsIntegration";
 import { Sql } from "../src/sql/types";
 import { createDbTunnel } from "../../shared/database/local/tunnel";
+import { getDatabaseConnection } from "../../shared/database/databaseConnection";
 
 const dynamo = new AWS.DynamoDB.DocumentClient(standardAwsConfig);
 
-type User = Record<string, unknown>;
-export async function getUsersToMigrate(): Promise<User[]> {
-  const getStoredUsers = async (
+async function getDynamoRows(
+  TableName: string
+): Promise<DocumentClient.AttributeMap[]> {
+  const getRows = async (
     startKey?: DocumentClient.Key
-  ): Promise<User[]> => {
+  ): Promise<DocumentClient.AttributeMap[]> => {
     const userResults = await dynamo
       .scan({
-        TableName: "pinboard-CODE-pinboardusertable2621B03F-16NOYN7UMQ1RS",
+        TableName,
         ExclusiveStartKey: startKey,
       })
       .promise();
@@ -22,81 +23,122 @@ export async function getUsersToMigrate(): Promise<User[]> {
     const storedUsers = userResults.Items || [];
 
     if (userResults.LastEvaluatedKey) {
-      return [
-        ...storedUsers,
-        ...(await getStoredUsers(userResults.LastEvaluatedKey)),
-      ];
+      return [...storedUsers, ...(await getRows(userResults.LastEvaluatedKey))];
     } else {
       return storedUsers;
     }
   };
 
-  return getStoredUsers();
+  return getRows();
 }
 
-export async function getItemsToMigrate(): Promise<User[]> {
-  const getStoredItems = async (
-    startKey?: DocumentClient.Key
-  ): Promise<User[]> => {
-    const itemsResults = await dynamo
-      .scan({
-        TableName: "pinboard-CODE-pinboarditemtable83382753-1QVZDRAX9CZ3I",
-        ExclusiveStartKey: startKey,
-      })
-      .promise();
-
-    const items = itemsResults.Items || [];
-
-    if (itemsResults.LastEvaluatedKey) {
-      return [
-        ...items,
-        ...(await getStoredItems(itemsResults.LastEvaluatedKey)),
-      ];
-    } else {
-      return items;
-    }
-  };
-
-  return getStoredItems();
-}
-
-export async function migrateUsers(sql: Sql) {
-  const users = await getUsersToMigrate();
+export async function migrateUsers(sql: Sql, tableName: string) {
+  const users = await getDynamoRows(tableName);
   console.log("NUMBER TO WRITE", users.length);
   return Promise.allSettled(
-    users.map(
-      ({ ttlEpochSeconds, manuallyOpenedPinboardIds, ...user }) =>
-        sql`INSERT INTO "User" ${sql({
+    users
+      .filter(({ firstName, lastName }) => firstName && lastName)
+      .map(
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        ({ ttlEpochSeconds, manuallyOpenedPinboardIds, ...user }) =>
+          sql`
+        INSERT INTO "User" ${sql({
           ...user,
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
           //@ts-ignore
           manuallyOpenedPinboardIds: manuallyOpenedPinboardIds?.values || null,
-        })}`
-    )
+        })}
+        ON CONFLICT ("email") DO NOTHING`
+      )
   );
 }
 
-export async function migrateItems(sql: Sql) {
-  const items = await getItemsToMigrate();
+export async function migrateItemsAndLastItemSeenByUser(
+  sql: Sql,
+  itemTableName: string,
+  lastItemSeenByUserTableName: string
+) {
+  const items = await getDynamoRows(itemTableName);
+  const lastItemSeenByUsers = await getDynamoRows(lastItemSeenByUserTableName);
+  if ((await sql`SELECT COUNT(*) AS count FROM "Item"`)[0].count > 0) {
+    throw new Error("The 'Item' table is not empty. Aborting migration.");
+  }
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   items.sort((a, b) => a.timestamp - b.timestamp);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //@ts-ignore
   for (const { id, timestamp, user, seenBy, ...item } of items) {
-    if (!item.userEmail) continue;
+    if (!item.userEmail) {
+      continue;
+    }
+    const newStyleID = (
+      await sql`
+        INSERT INTO "Item" ${sql({
+          ...item,
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          //@ts-ignore
+          timestamp: new Date(timestamp * 1000),
+        })}
+        ON CONFLICT ("id") DO NOTHING
+        RETURNING "id"
+    `
+    )[0].id;
     console.log(
-      await sql`INSERT INTO "Item" ${sql({
-        ...item,
-        legacyId: id,
-        //@ts-ignore
-        timestamp: new Date(timestamp * 1000),
-      })}`
+      "migrated Item with id",
+      id,
+      "- now has new style id",
+      newStyleID
+    );
+    const matchingLastItemSeenByUsers = lastItemSeenByUsers.filter(
+      ({ itemID }) => itemID === id
+    );
+    console.log(
+      await Promise.all(
+        matchingLastItemSeenByUsers.map(
+          (lastItemSeenByUser) =>
+            sql`
+        INSERT INTO "LastItemSeenByUser" ${sql({
+          ...lastItemSeenByUser,
+          seenAt: new Date(lastItemSeenByUser.seenAt * 1000),
+          itemID: newStyleID,
+        })}
+      `
+        )
+      )
     );
   }
 }
 
 (async () => {
-  await createDbTunnel();
+  const DYNAMO_TABLE_NAMES = {
+    CODE: {
+      User: "pinboard-CODE-pinboardusertable2621B03F-16NOYN7UMQ1RS",
+      Item: "pinboard-CODE-pinboarditemtable83382753-1QVZDRAX9CZ3I",
+      LastItemSeenByUser:
+        "pinboard-CODE-pinboardlastitemseenbyusertable132BE99C-15W8MHP4HJB1N",
+    },
+    PROD: {
+      User: "pinboard-PROD-pinboardusertable2621B03F-108EXGU72O7BW",
+      Item: "pinboard-PROD-pinboarditemtable83382753-1LQARAGXCSLI8",
+      LastItemSeenByUser:
+        "pinboard-PROD-pinboardlastitemseenbyusertable132BE99C-DFE4L38U1XBV",
+    },
+  };
+
+  const stage: "CODE" | "PROD" = await createDbTunnel();
 
   const sql = await getDatabaseConnection();
 
-  console.log(await migrateItems(sql));
+  const dynamoTableNames = DYNAMO_TABLE_NAMES[stage];
+
+  console.log(await migrateUsers(sql, dynamoTableNames.User));
+
+  console.log(
+    await migrateItemsAndLastItemSeenByUser(
+      sql,
+      dynamoTableNames.Item,
+      dynamoTableNames.LastItemSeenByUser
+    )
+  );
 })();

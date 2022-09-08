dd49e3fb27308da2dd1f69687691f0847513d3ff
Phill Barron <phillip.barron@guardian.co.uk>
2022-09-08 11:05:52 +0100

    added foreign key constrain from lastItemSeenByUser to itemId

--- database-bridge-lambda/migration/003-LastItemSeenByUserTable.sql
+++ database-bridge-lambda/migration/003-LastItemSeenByUserTable.sql
@@ -1,6 +1,8 @@
 CREATE TABLE "LastItemSeenByUser" (
       "pinboardId" varchar(128) NOT NULL,
-      "itemID" INT NOT NULL,
+      "itemID" INT NOT NULL
+          CONSTRAINT lastitemseenbyuser_item_id_fk
+              REFERENCES "Item",
       "userEmail" varchar(512) NOT NULL,
       "seenAt" TIMESTAMP NOT NULL,
       PRIMARY KEY ("pinboardId", "userEmail")

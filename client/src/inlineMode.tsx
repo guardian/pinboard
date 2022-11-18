60fa40bd691df38973618c9845516324340f183a
Tom Richards <tom.richards@guardian.co.uk>
2022-11-18 10:50:50 +0000

    WIP use dedicated pinboard column placeholder in workflow (`.content-list-item__field--pinboard`)
    Co-authored-by: Ara Cho <47318984+aracho1@users.noreply.github.com>
    Co-authored-by: Tom Richards <tom.richards@guardian.co.uk>
    Co-authored-by: Phill Barron <phillip.barron@guardian.co.uk>

--- client/src/inlineMode.tsx
+++ client/src/inlineMode.tsx
@@ -1,15 +1,21 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import { InlinePinboardTogglePortal } from "./inlinePinboardToggle";
+import {
+  INLINE_TOGGLE_WIDTH,
+  InlinePinboardTogglePortal,
+} from "./inlinePinboardToggle";
 import { useLazyQuery } from "@apollo/client";
 import { gqlGetItemCounts } from "../gql";
 import { PinboardIdWithItemCounts } from "../../shared/graphql/graphql";
 import { css } from "@emotion/react";
-import { neutral, space } from "@guardian/source-foundations";
+import { neutral } from "@guardian/source-foundations";
 import { boxShadow } from "./styling";
+import { pinboard } from "../colours";
 import { Pinboard } from "./pinboard";
 
 export const WORKFLOW_TITLE_QUERY_SELECTOR =
-  ".content-list-item__field--priority";
+  ".content-list-item__field--pinboard";
+
+export const INLINE_PANEL_WIDTH = 260;
 
 interface InlineModeProps {
   workflowTitleElements: HTMLElement[];
@@ -19,7 +25,7 @@ export const InlineMode = ({ workflowTitleElements }: InlineModeProps) => {
   const workflowTitleElementLookup = useMemo(
     () =>
       workflowTitleElements.reduce((acc, node) => {
-        const pinboardId = node.parentElement?.id?.replace("stub-", "");
+        const { pinboardId } = node.dataset;
         return pinboardId ? { ...acc, [pinboardId]: node } : acc;
       }, {} as Record<string, HTMLElement>),
     [workflowTitleElements]
@@ -36,22 +42,24 @@ export const InlineMode = ({ workflowTitleElements }: InlineModeProps) => {
     }
   }, [workflowTitleElementLookup]);
 
-  const itemCounts: PinboardIdWithItemCounts[] =
-    itemCountsQuery.data?.getItemCounts || [];
-
-  const itemCountsLookup = itemCounts.reduce(
-    (lookup, element: PinboardIdWithItemCounts) => ({
-      ...lookup,
-      [element.pinboardId]: element,
-    }),
-    {} as Record<string, PinboardIdWithItemCounts>
+  const itemCountsLookup = useMemo(
+    () =>
+      (
+        (itemCountsQuery.data?.getItemCounts as PinboardIdWithItemCounts[]) ||
+        []
+      ).reduce(
+        (lookup, element: PinboardIdWithItemCounts) => ({
+          ...lookup,
+          [element.pinboardId]: element,
+        }),
+        {} as Record<string, PinboardIdWithItemCounts>
+      ),
+    [itemCountsQuery.data]
   );
 
   const [maybeSelectedPinboardId, setMaybeSelectedPinboardId] = useState<
     string | null
   >(null);
-
-  const panelRef = useRef(null);
 
   return (
     <React.Fragment>
@@ -65,33 +73,55 @@ export const InlineMode = ({ workflowTitleElements }: InlineModeProps) => {
             isLoading={itemCountsQuery.loading}
             isSelected={pinboardId === maybeSelectedPinboardId}
             setMaybeSelectedPinboardId={setMaybeSelectedPinboardId}
+            offsetTop={node.offsetTop}
           />
         )
       )}
-      {maybeSelectedPinboardId && (
-        <div
-          ref={panelRef}
-          css={css`
-            position: absolute;
-            display: flex;
-            flex-direction: column;
-            z-index: 99998;
-            top: ${space[5]}px;
-            height: calc(100vh - 400px);
-            margin-left: 250px;
-            background: ${neutral[93]};
-            box-shadow: ${boxShadow};
-            width: 260px;
-          `}
-        >
-          <Pinboard
-            pinboardId={maybeSelectedPinboardId}
-            isSelected
-            isExpanded
-            panelElement={panelRef.current}
-          />
-        </div>
-      )}
     </React.Fragment>
+  );
+};
+
+interface InlineModePanelProps {
+  pinboardId: string;
+  offsetTop: number;
+}
+
+export const InlineModePanel = ({
+  pinboardId,
+  offsetTop,
+}: InlineModePanelProps) => {
+  const panelRef = useRef(null);
+  const marginTop = useMemo(
+    () =>
+      50 +
+      (document.getElementById("scrollable-area")?.scrollTop || 0) -
+      offsetTop,
+    []
+  );
+  return (
+    <div
+      ref={panelRef}
+      css={css`
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        z-index: 3;
+        margin-top: ${marginTop}px;
+        height: calc(100vh - 200px);
+        margin-left: ${INLINE_TOGGLE_WIDTH + 25}px;
+        background: ${neutral[93]};
+        box-shadow: ${boxShadow};
+        width: ${INLINE_PANEL_WIDTH}px;
+        border: 3px solid ${pinboard["500"]};
+        border-radius: 5px;
+      `}
+    >
+      <Pinboard
+        pinboardId={pinboardId}
+        isSelected
+        isExpanded
+        panelElement={panelRef.current}
+      />
+    </div>
   );
 };
--- client/src/inlinePinboardToggle.tsx
+++ client/src/inlinePinboardToggle.tsx
@@ -6,6 +6,10 @@ import { composer, pinboard, pinMetal } from "../colours";
 import { PinboardIdWithItemCounts } from "../../shared/graphql/graphql";
 import { SvgSpinner } from "@guardian/source-react-components";
 import { palette, space } from "@guardian/source-foundations";
+import { agateSans } from "../fontNormaliser";
+import { INLINE_PANEL_WIDTH, InlineModePanel } from "./inlineMode";
+
+export const INLINE_TOGGLE_WIDTH = 50;
 
 interface InlinePinboardToggleProps {
   pinboardId: string;
@@ -13,6 +17,7 @@ interface InlinePinboardToggleProps {
   isLoading: boolean;
   isSelected: boolean;
   setMaybeSelectedPinboardId: (newId: string | null) => void;
+  offsetTop: number;
 }
 
 const InlinePinboardToggle = ({
@@ -21,48 +26,73 @@ const InlinePinboardToggle = ({
   isLoading,
   isSelected,
   setMaybeSelectedPinboardId,
+  offsetTop,
 }: InlinePinboardToggleProps) => (
   <root.div
-    style={{ display: "inline-block", width: "100%" }}
     onClickCapture={(event) => {
       event.stopPropagation();
       setMaybeSelectedPinboardId(isSelected ? null : pinboardId);
     }}
   >
+    {isSelected && (
+      <InlineModePanel pinboardId={pinboardId} offsetTop={offsetTop} />
+    )}
     <div
       css={css`
-        text-align: right;
+        ${agateSans.xxsmall()};
+        font-size: 11px;
+        display: inline-flex;
+        align-items: center;
+        justify-content: flex-end;
         white-space: nowrap;
-        background-color: ${pinboard[isSelected ? "800" : "500"]};
+        background-color: ${pinboard["500"]};
         color: ${pinMetal};
-        padding: 2px 3px;
         border-radius: ${space[1]}px;
-        width: 100%;
+
+        &:hover {
+          background-color: ${pinboard[isSelected ? "500" : "800"]};
+        }
+
+        min-width: ${INLINE_TOGGLE_WIDTH}px;
+        padding: 2px ${isSelected ? 25 : 3}px 2px 3px;
+        margin-right: ${isSelected ? INLINE_PANEL_WIDTH + 15 : 0}px;
+        position: relative;
+        ${isSelected ? "z-index: 3" : ""}
       `}
     >
       {isLoading ? (
         <SvgSpinner size="xsmall" />
       ) : (
         <span>
-          {!!counts?.unreadCount && (
+          {counts?.unreadCount ? (
             <span>
               <div
                 css={css`
                   display: inline-block;
-                  border-radius: 50%;
+                  border-radius: 10px;
                   background-color: ${composer.warning[300]};
-                  padding: 1px 3px;
-                  min-width: 10px;
+                  padding: 0 2px;
+                  margin: 1px;
+                  min-width: 14px;
                   color: ${palette.neutral[100]};
                   text-align: center;
+                  font-weight: bold;
                 `}
               >
                 {counts.unreadCount}
               </div>{" "}
-              of{" "}
+              of
             </span>
-          )}
-          {counts?.totalCount || 0}
+          ) : (
+            "total"
+          )}{" "}
+          <span
+            css={css`
+              font-weight: bold;
+            `}
+          >
+            {counts?.totalCount || 0}
+          </span>
         </span>
       )}
     </div>

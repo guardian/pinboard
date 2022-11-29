e28d6af95096c3ae298aef5f8bc0ce1741873b7b
Ara Cho <ara.cho@guardian.co.uk>
2022-11-29 23:59:30 +0000

    add typescript to jest config and WIP make the test pass

--- bootstrapping-lambda/src/reporting/queryMappingService.ts
+++ bootstrapping-lambda/src/reporting/queryMappingService.ts
@@ -2,5 +2,15 @@ import { GrafanaRequest } from "./grafanaType";
 import { AppSyncQuery } from "../appSyncClient";
 
 export const mapQuery = (request: GrafanaRequest): AppSyncQuery => {
-  return {} as AppSyncQuery;
+  const { range, targets } = request;
+
+  return {
+    query:
+      "query MyQuery($range: Range, $metric: String) {getMetricsForRange(range: $range, metric: $metric) {}}",
+    variables: {
+      range: range,
+      metric: targets[0].target,
+    },
+    operation: "MyQuery",
+  } as AppSyncQuery;
 };

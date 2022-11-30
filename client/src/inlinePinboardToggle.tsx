3c12e4ab69a008f7ce494def03538a66c6ae5534
Tom Richards <tom.richards@guardian.co.uk>
2022-11-30 14:26:11 +0000

    clear unread flag in workflow ASAP (rather than waiting for the polling, since we have `unreadFlags` available anyway)

--- client/src/inlinePinboardToggle.tsx
+++ client/src/inlinePinboardToggle.tsx
@@ -8,6 +8,7 @@ import { SvgSpinner } from "@guardian/source-react-components";
 import { palette, space } from "@guardian/source-foundations";
 import { agateSans } from "../fontNormaliser";
 import { INLINE_PANEL_WIDTH, InlineModePanel } from "./inlineMode";
+import { useGlobalStateContext } from "./globalState";
 
 export const INLINE_TOGGLE_WIDTH = 50;
 
@@ -25,75 +26,78 @@ const InlinePinboardToggle = ({
   isLoading,
   isSelected,
   setMaybeSelectedPinboardId,
-}: InlinePinboardToggleProps) => (
-  <root.div
-    onClick={(event) => {
-      event.stopPropagation();
-      setMaybeSelectedPinboardId(isSelected ? null : pinboardId);
-    }}
-  >
-    {isSelected && <InlineModePanel pinboardId={pinboardId} />}
-    <div
-      css={css`
-        ${agateSans.xxsmall()};
-        font-size: 11px;
-        display: inline-flex;
-        align-items: center;
-        justify-content: flex-end;
-        white-space: nowrap;
-        background-color: ${pinboard["500"]};
-        color: ${pinMetal};
-        border-radius: ${space[1]}px;
-
-        &:hover {
-          background-color: ${pinboard[isSelected ? "500" : "800"]};
-        }
-
-        min-width: ${INLINE_TOGGLE_WIDTH}px;
-        padding: 2px ${isSelected ? 25 : 3}px 2px 3px;
-        margin-right: ${isSelected ? INLINE_PANEL_WIDTH + 15 : 0}px;
-        position: relative;
-        ${isSelected ? "z-index: 3" : ""}
-      `}
+}: InlinePinboardToggleProps) => {
+  const { unreadFlags } = useGlobalStateContext();
+  return (
+    <root.div
+      onClick={(event) => {
+        event.stopPropagation();
+        setMaybeSelectedPinboardId(isSelected ? null : pinboardId);
+      }}
     >
-      {isLoading ? (
-        <SvgSpinner size="xsmall" />
-      ) : (
-        <span>
-          {counts?.unreadCount ? (
-            <span>
-              <div
-                css={css`
-                  display: inline-block;
-                  border-radius: 10px;
-                  background-color: ${composer.warning[300]};
-                  padding: 0 2px;
-                  margin: 1px;
-                  min-width: 14px;
-                  color: ${palette.neutral[100]};
-                  text-align: center;
-                  font-weight: bold;
-                `}
-              >
-                {counts.unreadCount}
-              </div>{" "}
-              of
+      {isSelected && <InlineModePanel pinboardId={pinboardId} />}
+      <div
+        css={css`
+          ${agateSans.xxsmall()};
+          font-size: 11px;
+          display: inline-flex;
+          align-items: center;
+          justify-content: flex-end;
+          white-space: nowrap;
+          background-color: ${pinboard["500"]};
+          color: ${pinMetal};
+          border-radius: ${space[1]}px;
+
+          &:hover {
+            background-color: ${pinboard[isSelected ? "500" : "800"]};
+          }
+
+          min-width: ${INLINE_TOGGLE_WIDTH}px;
+          padding: 2px ${isSelected ? 25 : 3}px 2px 3px;
+          margin-right: ${isSelected ? INLINE_PANEL_WIDTH + 15 : 0}px;
+          position: relative;
+          ${isSelected ? "z-index: 3" : ""}
+        `}
+      >
+        {isLoading ? (
+          <SvgSpinner size="xsmall" />
+        ) : (
+          <span>
+            {counts?.unreadCount && unreadFlags?.[pinboardId] !== false ? (
+              <span>
+                <div
+                  css={css`
+                    display: inline-block;
+                    border-radius: 10px;
+                    background-color: ${composer.warning[300]};
+                    padding: 0 2px;
+                    margin: 1px;
+                    min-width: 14px;
+                    color: ${palette.neutral[100]};
+                    text-align: center;
+                    font-weight: bold;
+                  `}
+                >
+                  {counts.unreadCount}
+                </div>{" "}
+                of
+              </span>
+            ) : (
+              "total"
+            )}{" "}
+            <span
+              css={css`
+                font-weight: bold;
+              `}
+            >
+              {counts?.totalCount || 0}
             </span>
-          ) : (
-            "total"
-          )}{" "}
-          <span
-            css={css`
-              font-weight: bold;
-            `}
-          >
-            {counts?.totalCount || 0}
           </span>
-        </span>
-      )}
-    </div>
-  </root.div>
-);
+        )}
+      </div>
+    </root.div>
+  );
+};
 
 interface InlinePinboardTogglePortalProps extends InlinePinboardToggleProps {
   node: HTMLElement;

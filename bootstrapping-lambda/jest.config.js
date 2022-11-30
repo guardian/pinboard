1d3f427624ca244b0ed007718feab1eea8b2dc4f
Phill Barron <phillip.barron@guardian.co.uk>
2022-11-30 11:20:45 +0000

    Added auth middleware
    Co-authored-by: Ara Cho <47318984+aracho1@users.noreply.github.com>
    Co-authored-by: Tom Richards <tom.richards@guardian.co.uk>
    Co-authored-by: Phill Barron <phillip.barron@guardian.co.uk>

---
+++ bootstrapping-lambda/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testMatch: ["**/*.test.ts"],
+  transform: {
+    "^.+\\.(j|t)sx?$": "ts-jest",
+    "^.+\\.svg$": "<rootDir>/fileTransformer.js",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/fileTransformer.js",
+  },
+  testEnvironment: "jest-environment-node",
+};

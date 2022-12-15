365d6ad744dab5dd28350919842fe769c46d4222
Phill Barron <phillip.barron@guardian.co.uk>
2022-12-15 09:54:36 +0000

    WIP - invoking DB lambda directly from bootstrapping lambda for grafana call

---
+++ bootstrapping-lambda/src/reporting/reportingServiceClient.ts
@@ -0,0 +1,19 @@
+import { GrafanaRequest } from "../../../shared/types/grafanaType";
+import * as AWS from "aws-sdk";
+import { standardAwsConfig } from "../../../shared/awsIntegration";
+import { Stage } from "../../../shared/types/stage";
+
+export const getMetrics = async (request: GrafanaRequest) => {
+  const stage = (process.env.STAGE as Stage) || "CODE";
+  const lambda = new AWS.Lambda(standardAwsConfig);
+  const response = await lambda
+    .invoke({
+      FunctionName: `pinboard-database-bridge-lambda-${stage}`,
+      Payload: JSON.stringify(request),
+    })
+    .promise();
+
+  const parsedResponse = JSON.parse(response.Payload as string);
+  console.log(parsedResponse);
+  return parsedResponse;
+};

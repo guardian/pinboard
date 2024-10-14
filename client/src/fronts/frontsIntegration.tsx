719bd9b7eec59e25189c52c049db92252545c80c
Tom Richards <tom.richards@guardian.co.uk>
2024-10-14 11:17:08 +0100

    display draggable thumbs when the `<pinboard-article-button>` has a `data-with-draggable-thumbs-of-ratio` attribute

--- client/src/fronts/frontsIntegration.tsx
+++ client/src/fronts/frontsIntegration.tsx
@@ -1,101 +1,34 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { useGlobalStateContext } from "../globalState";
 import { PinboardData } from "shared/graphql/extraTypes";
 import { PinboardIdWithItemCounts } from "shared/graphql/graphql";
 import ReactDOM from "react-dom";
-import root from "react-shadow/emotion";
-import { css } from "@emotion/react";
 import { useApolloClient } from "@apollo/client";
 import { gqlGetItemCounts, gqlGetPinboardsByPaths } from "../../gql";
-import { pinboard } from "../../colours";
-import { neutral } from "@guardian/source-foundations";
-import { ButtonInOtherTools } from "../buttonInOtherTools";
+import { FrontsPinboardArticleButton } from "./frontsPinboardArticleButton";
 
 export const FRONTS_PINBOARD_ELEMENTS_QUERY_SELECTOR =
   "pinboard-article-button";
-
-interface PinboardArticleButtonProps {
-  maybePinboardData: PinboardData | undefined;
-  maybeItemCounts: PinboardIdWithItemCounts | undefined;
-}
-const PinboardArticleButton = ({
-  maybePinboardData,
-  maybeItemCounts,
-}: PinboardArticleButtonProps) => {
-  const { setIsExpanded, openPinboard } = useGlobalStateContext();
-
-  return (
-    <root.span>
-      <ButtonInOtherTools
-        extraCss={css`
-          display: inline-flex;
-          background-color: ${maybePinboardData
-            ? pinboard[500]
-            : neutral["60"]};
-        `}
-        onClick={(event) => {
-          event.stopPropagation();
-          if (maybePinboardData) {
-            setIsExpanded(true);
-            openPinboard(false)(maybePinboardData, false); // TODO probably should be 'peek at pinboard' from panel.tsx
-          } else {
-            alert(
-              "This piece is not tracked in workflow, this is needed to chat and share assets such as crops via Pinboard."
-            );
-            // TODO consider offering an option to create workflow tracking
-          }
-        }}
-      >
-        {!maybePinboardData && "not tracked in workflow"}
-        {maybeItemCounts && (
-          <>
-            {
-              maybeItemCounts.unreadCount ||
-                null /*TODO display number in red blob*/
-            }
-            {maybeItemCounts.unreadCount > 0 && " unread of "}
-            {maybeItemCounts.totalCount}
-            {" items "}
-            {maybeItemCounts.totalCropCount > 0 && (
-              <>
-                {" with "}
-                {maybeItemCounts.totalCropCount}
-                {" crops "}
-                {(maybeItemCounts.fiveByFourCount > 0 ||
-                  maybeItemCounts.fourByFiveCount > 0) &&
-                  " (incl."}
-                {maybeItemCounts.fiveByFourCount > 0 &&
-                  `${maybeItemCounts.fiveByFourCount} at 5:4`}
-                {maybeItemCounts.fourByFiveCount > 0 &&
-                  `${maybeItemCounts.fourByFiveCount} at 4:5`}
-                {(maybeItemCounts.fiveByFourCount > 0 ||
-                  maybeItemCounts.fourByFiveCount > 0) &&
-                  ")"}
-              </>
-            )}
-          </>
-        )}
-      </ButtonInOtherTools>
-    </root.span>
-  );
-};
 
 export const FrontsIntegration = ({
   frontsPinboardElements,
 }: {
   frontsPinboardElements: HTMLElement[];
 }) => {
-  const pathToElementMap: { [path: string]: HTMLElement } = useMemo(
+  const pathToElementsMap: { [path: string]: HTMLElement[] } = useMemo(
     () =>
       frontsPinboardElements.reduce(
+        // TODO could be replaced with groupBy if we upgrade to latest ES in tsconfig
         (acc, htmlElement) =>
           htmlElement.dataset.urlPath
             ? {
                 ...acc,
-                [htmlElement.dataset.urlPath]: htmlElement,
+                [htmlElement.dataset.urlPath]: [
+                  ...(acc[htmlElement.dataset.urlPath] || []),
+                  htmlElement,
+                ],
               }
             : acc,
-        {} as Record<string, HTMLElement>
+        {} as Record<string, HTMLElement[]>
       ),
     [frontsPinboardElements]
   );
@@ -106,7 +39,7 @@ export const FrontsIntegration = ({
     {} as { [path: string]: PinboardData }
   );
   useEffect(() => {
-    const paths = Object.keys(pathToElementMap);
+    const paths = Object.keys(pathToElementsMap);
     paths.length > 0 &&
       apolloClient
         .query({
@@ -126,7 +59,7 @@ export const FrontsIntegration = ({
             );
         });
     //TODO handle errors
-  }, [pathToElementMap]);
+  }, [pathToElementsMap]);
 
   interface ItemCountsLookup {
     [pinboardId: string]: PinboardIdWithItemCounts;
@@ -166,17 +99,23 @@ export const FrontsIntegration = ({
 
   return (
     <>
-      {Object.entries(pathToElementMap).map(([path, htmlElementToMountInto]) =>
-        ReactDOM.createPortal(
-          <PinboardArticleButton
-            maybePinboardData={pathToPinboardDataMap[path]}
-            maybeItemCounts={
-              pathToPinboardDataMap[path] &&
-              itemCountsLookup[pathToPinboardDataMap[path].id]
-            }
-          />,
-          htmlElementToMountInto
-        )
+      {Object.entries(pathToElementsMap).map(
+        ([path, htmlElementsToMountInto]) =>
+          htmlElementsToMountInto.map((htmlElementToMountInto) =>
+            ReactDOM.createPortal(
+              <FrontsPinboardArticleButton
+                maybePinboardData={pathToPinboardDataMap[path]}
+                maybeItemCounts={
+                  pathToPinboardDataMap[path] &&
+                  itemCountsLookup[pathToPinboardDataMap[path].id]
+                }
+                withDraggableThumbsOfRatio={
+                  htmlElementToMountInto.dataset.withDraggableThumbsOfRatio
+                }
+              />,
+              htmlElementToMountInto
+            )
+          )
       )}
     </>
   );

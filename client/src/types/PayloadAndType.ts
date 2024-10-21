6ea9742d712f33b85f111703a23855ae58b01dd8
Pete F <pete.faulconbridge@theguardian.com>
2024-10-21 13:17:31 +0100

    Remove export from non-imported types

--- client/src/types/PayloadAndType.ts
+++ client/src/types/PayloadAndType.ts
@@ -1,19 +1,17 @@
 import * as Sentry from "@sentry/react";
 
-export const sources = ["grid", "mam"] as const;
-export const sourceTypes = ["crop", "original", "search", "video"] as const;
+const sources = ["grid", "mam"] as const;
+const sourceTypes = ["crop", "original", "search", "video"] as const;
 
-export type Source = (typeof sources)[number];
-export const isSource = (source: unknown): source is Source =>
+type Source = (typeof sources)[number];
+const isSource = (source: unknown): source is Source =>
   sources.includes(source as Source);
-export type SourceType = (typeof sourceTypes)[number];
-export const isSourceType = (sourceType: unknown): sourceType is SourceType =>
+type SourceType = (typeof sourceTypes)[number];
+const isSourceType = (sourceType: unknown): sourceType is SourceType =>
   sourceTypes.includes(sourceType as SourceType);
 
 export type PayloadType = `${Source}-${SourceType}`; // TODO improve this type as it enumerates all the combinations, e.g. mam-original which is not valid
-export const isPayloadType = (
-  payloadType: string
-): payloadType is PayloadType => {
+const isPayloadType = (payloadType: string): payloadType is PayloadType => {
   const parts = payloadType.split("-");
   return parts.length === 2 && isSource(parts[0]) && isSourceType(parts[1]);
 };
@@ -34,11 +32,11 @@ export interface PayloadWithApiUrl extends PayloadCommon {
   apiUrl: string;
 }
 
-export type Payload =
+type Payload =
   | PayloadWithThumbnail
   | PayloadWithApiUrl
   | PayloadWithExternalUrl;
-export const isPayload = (maybePayload: unknown): maybePayload is Payload => {
+const isPayload = (maybePayload: unknown): maybePayload is Payload => {
   return (
     typeof maybePayload === "object" &&
     maybePayload !== null &&

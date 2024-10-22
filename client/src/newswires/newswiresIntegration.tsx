fc892f057cf4d5fda8c5dc3bbbfa220f45c47f4d
Pete F <pete.faulconbridge@theguardian.com>
2024-10-22 17:29:29 +0100

    WIP

--- client/src/newswires/newswiresIntegration.tsx
+++ client/src/newswires/newswiresIntegration.tsx
@@ -1,48 +1,52 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { debounce } from "../util";
 import React from "react";
-import { css } from "@emotion/react";
+import { css, Global } from "@emotion/react";
 import { useGlobalStateContext } from "../globalState";
+import { pinboard, pinMetal } from "../../colours";
+import { textSans } from "../../fontNormaliser";
+import { space } from "@guardian/source-foundations";
+import ReactDOM from "react-dom";
 
 const SELECTION_TARGET_DATA_ATTR = "[data-pinboard-selection-target]";
 
 export const NewswiresIntegration = () => {
-  const { setPayloadToBeSent } = useGlobalStateContext();
+  const { setPayloadToBeSent, setIsExpanded } = useGlobalStateContext();
   const [selectedHTML, setSelectedHTML] = useState<string | null>(null);
+  const [mountPoint, setMountPoint] = useState<Element | null>(null);
+  const [buttonCoords, setButtonCoords] = useState({ x: 0, y: 0 });
 
   const handleSelectionChange = () => {
     const selection = window.getSelection();
-    if (selection) {
+    if (selection && selection.rangeCount > 0) {
       const maybeOriginalTargetEl = document.querySelector(
         SELECTION_TARGET_DATA_ATTR
       );
-      try {
-        const clonedContents = selection.getRangeAt(0).cloneContents();
-        const maybeClonedTargetEl = clonedContents.querySelector(
-          SELECTION_TARGET_DATA_ATTR
+
+      setMountPoint(maybeOriginalTargetEl);
+      const clonedContents = selection.getRangeAt(0).cloneContents();
+      const maybeClonedTargetEl = clonedContents.querySelector(
+        SELECTION_TARGET_DATA_ATTR
+      );
+      if (maybeClonedTargetEl) {
+        console.log(
+          "selection contains whole target element; contents:",
+          maybeClonedTargetEl.innerHTML
         );
-        if (maybeClonedTargetEl) {
-          console.log(
-            "selection contains whole target element; contents:",
-            maybeClonedTargetEl.innerHTML
-          );
-          setSelectedHTML(maybeClonedTargetEl.innerHTML);
-        } else {
-          if (
-            maybeOriginalTargetEl?.contains(selection.anchorNode) &&
-            maybeOriginalTargetEl?.contains(selection.focusNode)
-          ) {
-            const tempEl = document.createElement("div");
-            tempEl.appendChild(clonedContents);
-            console.log(
-              "selection is within target element; contents:",
-              tempEl.innerHTML
-            );
-            setSelectedHTML(tempEl.innerHTML);
-          }
-        }
-      } catch (e) {
-        console.error("Error cloning selection contents", e);
+        setSelectedHTML(maybeClonedTargetEl.innerHTML);
+        setSelectionFocusNode(selectionFocusNode); // todo: set coords instead, based on selection
+      } else if (
+        maybeOriginalTargetEl?.contains(selection.anchorNode) &&
+        maybeOriginalTargetEl?.contains(selection.focusNode)
+      ) {
+        const tempEl = document.createElement("div");
+        tempEl.appendChild(clonedContents);
+        console.log(
+          "selection is within target element; contents:",
+          tempEl.innerHTML
+        );
+        setSelectedHTML(tempEl.innerHTML);
+        setSelectionFocusNode(selection.focusNode);
       }
     }
   };
@@ -81,22 +85,44 @@ export const NewswiresIntegration = () => {
           embeddableUrl: window.location.href,
         },
       });
+      setIsExpanded(true);
     }
   }, [selectedHTML, setPayloadToBeSent]);
 
   return (
-    <div
-      css={css`
-        position: fixed;
-        top: 0;
-        left: 0;
-      `}
-    >
-      {selectedHTML && (
-        <div>
-          <button onClick={addSelectionToPinboard}>Add to pinboard</button>
-        </div>
-      )}
-    </div>
+    <>
+      <Global
+        styles={css`
+          ${SELECTION_TARGET_DATA_ATTR}::selection {
+            background-color: ${pinboard[500]};
+            color: ${pinMetal};
+          }
+        `}
+      />
+      {selectedHTML &&
+        mountPoint &&
+        ReactDOM.createPortal(
+          <div>
+            <button
+              css={css`
+                display: flex;
+                align-items: center;
+                background-color: ${pinboard[500]};
+                ${textSans.xsmall()};
+                border: none;
+                border-radius: 100px;
+                padding: 0 ${space[2]}px 0 ${space[3]}px;
+                line-height: 2;
+                cursor: pointer;
+                color: ${pinMetal};
+              `}
+              onClick={addSelectionToPinboard}
+            >
+              Add selection to pinboard
+            </button>
+          </div>,
+          mountPoint
+        )}
+    </>
   );
 };

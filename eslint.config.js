5bc3503f3244eebf21747746ad2ea1722f7c8eec
Rik Roots <rik.roots@guardian.co.uk>
2024-12-30 16:07:16 +0000

    run yarn prettier-write locally

--- eslint.config.js
+++ eslint.config.js
@@ -1,54 +1,52 @@
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import reactPlugin from 'eslint-plugin-react';
-import nodePlugin from 'eslint-plugin-n';
-import flowtypePlugin from 'eslint-plugin-flowtype';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactPlugin from "eslint-plugin-react";
+import nodePlugin from "eslint-plugin-n";
+import flowtypePlugin from "eslint-plugin-flowtype";
 
 export default tseslint.config(
+  // Load in recommended eslint configs
+  eslint.configs.recommended,
 
-    // Load in recommended eslint configs
-    eslint.configs.recommended,
+  // Load in recommended tslint configs
+  tseslint.configs.recommended,
 
-    // Load in recommended tslint configs
-    tseslint.configs.recommended,
+  // Plugins and rules overrides
+  {
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      react: reactPlugin,
+      node: nodePlugin,
+      flowtype: flowtypePlugin,
+    },
+    rules: {
+      // Change 'off' to 'warn' to work out where cleanup is required
+      // Change 'off' to 'error' for show-stopper issues
 
-    // Plugins and rules overrides
-    {
-        plugins: {
-            '@typescript-eslint': tseslint.plugin,
-            'react': reactPlugin,
-            'node': nodePlugin,
-            'flowtype': flowtypePlugin,
-        },
-        rules: {
+      "@/no-undef": "off",
 
-            // Change 'off' to 'warn' to work out where cleanup is required
-            // Change 'off' to 'error' for show-stopper issues
+      "@typescript-eslint/ban-ts-comment": "off",
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-this-alias": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
 
-            '@/no-undef': 'off',
+      "no-cond-assign": "off",
+      "no-constant-binary-expression": "off",
+      "no-constant-condition": "off",
+      "no-empty": "off",
+      "no-fallthrough": "off",
+      "no-func-assign": "off",
+      "no-prototype-builtins": "off",
+      "no-redeclare": "off",
+      "no-undef": "off",
+      "no-unsafe-finally": "off",
+      "no-unsafe-optional-chaining": "off",
+      "no-useless-escape": "off",
 
-            '@typescript-eslint/ban-ts-comment': 'off',
-            '@typescript-eslint/no-require-imports': 'off',
-            '@typescript-eslint/no-this-alias': 'off',
-            '@typescript-eslint/no-unused-expressions': 'off',
-            '@typescript-eslint/no-unused-vars': 'off',
+      "node/no-unsupported-features/node-builtins": "off",
 
-            'no-cond-assign': 'off',
-            'no-constant-binary-expression': 'off',
-            'no-constant-condition': 'off',
-            'no-empty': 'off',
-            'no-fallthrough': 'off',
-            'no-func-assign': 'off',
-            'no-prototype-builtins': 'off',
-            'no-redeclare': 'off',
-            'no-undef': 'off',
-            'no-unsafe-finally': 'off',
-            'no-unsafe-optional-chaining': 'off',
-            'no-useless-escape': 'off',
-
-            'node/no-unsupported-features/node-builtins': 'off',
-
-            'react/display-name': 'off',
-        }
-    }
+      "react/display-name": "off",
+    },
+  }
 );

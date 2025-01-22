44327e5fddb832fb4cc48e57b806e0d6a3144020
Rhys Mills <rhys.mills@guardian.co.uk>
2025-01-22 10:04:02 +0000

    Add comments explaining configuration options for Jest

--- jest.config.js
+++ jest.config.js
@@ -9,10 +9,12 @@ module.exports = {
   },
   testEnvironment: "jest-environment-node",
   snapshotFormat: {
+    // Use pre v28 Jest snapshot defaults, to prevent incompatible snapshots
     escapeString: true,
     printBasicPrototype: true,
   },
   moduleNameMapper: {
+    // Force module uuid to resolve with the CJS entry point, because Jest does not support package.json.exports. See https://github.com/uuidjs/uuid/issues/451
     "^uuid$": require.resolve("uuid"),
   },
 };

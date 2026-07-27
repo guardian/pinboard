04858edc93f1e28320c460a87b21c6351985477f
Liam Duffy <3338808+liamness@users.noreply.github.com>
2026-07-27 17:05:09 +0100

    WIP trying to use @guardian/permissions-client

--- bootstrapping-lambda/src/server.ts
+++ bootstrapping-lambda/src/server.ts
@@ -21,7 +21,7 @@ import {
 } from "./middleware/auth-middleware";
 
 import { getMetrics } from "./reporting/reportingServiceClient";
-import { listUserPermissions } from "shared/permissions";
+import { permissionsClient } from "shared/permissions";
 
 const s3 = new S3(standardAwsConfig);
 
@@ -135,7 +135,9 @@ server.get(
           appSyncConfig,
           userEmail: request.userEmail!,
           stage: (process.env.STAGE as Stage) || "LOCAL",
-          permissions: await listUserPermissions(request.userEmail!),
+          permissions: await permissionsClient.listUserPermissions(
+            request.userEmail!
+          ),
         },
         mainJsFilename,
         request.hostname
